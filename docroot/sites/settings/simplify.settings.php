92d5006ee1bb69c44d9d4ef65fc2b589779fa84b
William Ortega <william.es@squeaky.com>
2020-06-11 19:00:59 -0600

    Add second database configuration to acquia

---
+++ docroot/sites/settings/simplify.settings.php
@@ -0,0 +1,6 @@
+<?php
+
+// Add Simplify database created on Acquia Cloud as second database.
+if (file_exists('/var/www/site-php')) {
+  require '/var/www/site-php/s5ee0cd9b44e38/simplify-settings.inc';
+}
